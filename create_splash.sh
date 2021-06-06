27363df6be760a79c2f32e66221df849fffe65e0
Martin Raiber <martin@urbackup.org>
2021-06-06 17:46:30 +0200

    Update create_splash.sh

--- create_splash.sh
+++ create_splash.sh
@@ -8,7 +8,6 @@ echo "Version: $VERSION" >> splash.txt
 echo "Created: $CDATE" >> splash.txt
 
 mkdir -p image_root/config/includes.binary/isolinux
-convert -list font
 convert logo1-3-cd2.png -font Liberation-Sans -pointsize 22 -gravity NorthWest -annotate +350+100 '@splash.txt' image_root/config/includes.binary/isolinux/splash.png
 
 
